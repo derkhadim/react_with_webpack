5dfc0cccbf1a11070db15e90843bfb90c6847da7
derkhadim <dermouhamadou@gmail.com>
2017-06-06 13:07:15 +0000

    Setting up react with webpack and yarn

---
+++ client/Components/App/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class App extends React.Component {
+	render() {
+		const style = {textAlign: 'center'}
+		return(
+			<div style={style}>
+				<h1>Hello React</h1>
+			</div>
+		)
+	}
+}
